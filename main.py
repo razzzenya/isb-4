108c24446435b6217259cb4377d2cbe6aa170982
razzzenya <buckingkiska@gmail.com>
2023-05-13 19:06:44 +0400

    Были добавлены файлы: file_manager.py , main.py. Также были добавлены файлы с БИН'ом , хэшем и последними 4 цифрами. Написан файл настроек.

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from file_manager import FileManager
+
+SETTINGS_FILE = "files/settings.json"
+
+
+class AppGUI(QWidget):
+
+    def __init__(self, file_manager) -> None:
+        super().__init__()
+        self.initUI()
+        self.file_manager = file_manager
+
+    def initUI(self) -> None:
+        self.setWindowTitle("Card number finder application")
+        self.resize(500, 500)
+        load_settings_btn = QPushButton("&Load settings", self)
+        load_settings_btn.setToolTip("Loads .json settings file")
+        load_settings_btn.clicked.connect(self.select_settings_file)
+        check_card_btn = QPushButton("&Check card number for validity", self)
+        check_card_btn.move(0, 30)
+        check_card_btn.clicked.connect(self.check_card_number)
+        recover_number_btn = QPushButton("&Recover bank card number", self)
+        recover_number_btn.move(0, 60)
+        recover_number_btn.clicked.connect(self.recover_number)
+        quit_btn = QPushButton("&Quit", self)
+        quit_btn.move(210, 470)
+        quit_btn.clicked.connect(QCoreApplication.instance().quit)
+
+    def select_settings_file(self) -> None:
+        """Method that initializes settings into a class card_number_finder
+        """
+        try:
+            path = QFileDialog.getOpenFileName(
+                self, "Select file", filter="*.json")
+            done_msg = QMessageBox()
+            done_msg.setWindowTitle("Info message")
+            done_msg.setText(
+                f"Settings file successfully loaded from file {path}")
+            done_msg.setIcon(QMessageBox.Information)
+            done_msg.exec_()
+            self.file_manager.settings_path = path
+        except OSError:
+            QMessageBox.information(
+                self, "Settings", f"Settings file was not loaded from file {path}." f"The default path was applied.\nPath: {SETTINGS_FILE}")
+
+    def check_card_number(self) -> None:
+        if not self.file_manager.are_settings_loaded:
+            self.select_settings_file()
+        pass
+
+    def recover_number(self) -> None:
+        if not self.file_manager.are_settings_loaded:
+            self.select_settings_file()
+        pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    file_manager = FileManager()
+    window = AppGUI(file_manager)
+    window.show()
+    sys.exit(app.exec_())
