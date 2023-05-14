9cff108d6a1107ba92577824a98d5feb1747e74f
razzzenya <buckingkiska@gmail.com>
2023-05-14 16:31:04 +0400

    Был добавлен прогрессбар

--- card_number_recover.py
+++ card_number_recover.py
@@ -1,8 +1,9 @@
 import hashlib
 import multiprocessing as mp
+from typing import Callable
 
 
-def recover_card_num(hash: str, last_symbols: str, bin: str, cores: int = mp.cpu_count()) -> str:
+def recover_card_num(hash: str, last_symbols: str, bin: str, cores: int = mp.cpu_count(), progress_callback: Callable[[int], None] = None) -> str:
     """Function that tries to recover the card number using hash brute force
 
     Args:
@@ -13,11 +14,13 @@ def recover_card_num(hash: str, last_symbols: str, bin: str, cores: int = mp.cpu
     Returns:
         str: if the card number cannot be found, then an empty string will be returned, otherwise the card number will be returned
     """
-    
+
     args = [(hash, f"{bin}{i}{last_symbols}") for i in range(100000, 1000000)]
     with mp.Pool(processes=cores) as p:
         results = p.starmap(check_hash, args)
         for index, result in enumerate(results):
+            if progress_callback:
+                progress_callback(index)
             if result:
                 return args[index][1]
     return ""
